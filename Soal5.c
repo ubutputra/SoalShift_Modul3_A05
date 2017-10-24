4a8d9cb3287bdf93730c5115af91faeb6d2782c8
Rendoru <maulana.firman56@gmail.com>
2017-10-24 14:27:35 +0700

    Final Fix - No Mutex

--- Soal5.c
+++ Soal5.c
@@ -1,20 +1,23 @@
 
 #include <string.h>
 #include <pthread.h>
-#include <cstdlib>
 #include <stdio.h>
-char globalIterator[1001];
-char EndOfFile = 0;
-int FinishedThread=0,avaiableThread=0;
+
+char globalIterator[1500];
+pthread_t threadID[101];
+
 void* StringCounter(void* arg);
-pthread_mutex_t lock;
+
 int main(int argc, char* argv[]) {
-    pthread_mutex_init(&lock,NULL);
-    avaiableThread = argc;
-    for(int i=0;i<argc;i++){
-        
+    FILE* source = fopen("novel.txt","r");
+    fscanf(source,"%[^\n]%*c",globalIterator);
+    for(int i=1;i<argc;i++){
+        pthread_create(&threadID[i],NULL,&StringCounter,argv[i]);
     }
-    pthread_mutex_destroy(&lock);
+    for(int i=1;i<argc;i++){
+        pthread_join(threadID[i],NULL);
+    }
+    fclose(source);
     return 0;
 }
 
@@ -22,18 +25,13 @@ void* StringCounter(void* arg){
     char* subString = (char*)arg;
     int subLength = strlen(subString);
     int count = 0;
-    while(!EndOfFile){
-        char* iterator = globalIterator;
-        while(iterator = strstr(iterator,subString)){
-            iterator+= subLength;
-            count++;
-        }
-        FinishedThread++;
-        while(FinishedThread<avaiableThread){
-            //Do Nothing...
-        }
-    }
     
+    char* iterator = globalIterator;
+    while(iterator = strstr(iterator,subString)){
+        iterator+= subLength;
+        count++;
+    }
+
     printf("%s : %d\n",subString,count);
     
 }
