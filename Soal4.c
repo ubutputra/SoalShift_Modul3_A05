7411102f7b00a83e3d3e0549234d185ca6550d9a
Rendoru <maulana.firman56@gmail.com>
2017-10-24 14:47:07 +0700

    Fixed memory allocation pointer

--- Soal4.c
+++ Soal4.c
@@ -10,7 +10,7 @@ void* Factorial(void* arg);
 int main(int argc, char *argv[]) {
     for(int i=1;i<argc;i++){
         int stringLenght = strlen(argv[i]);
-        unsigned long long int *parsedInt=malloc(sizeof(unsigned long long int));
+        unsigned long long int *parsedInt=(unsigned long long int*)malloc(sizeof(unsigned long long int));
         for(int s=0;s<stringLenght;s++){
             *parsedInt=(*parsedInt)*10 + (argv[i][s]-'0');
         }
@@ -23,8 +23,13 @@ int main(int argc, char *argv[]) {
 }
 
 void* Factorial(void* arg){
-    
-    
+    unsigned long long command = *((unsigned long long int*)arg),result=1,init;
+    init = command;
+    while(command){
+        result *= command--;
+    }
+    printf("Hasil %llu! = %llu\n",init,result);
+    free((unsigned long long int*)arg);
 }
 
 
