03e786cb5243c457bf0491397fce9e9c9f206a92
Rendoru <maulana.firman56@gmail.com>
2017-11-14 22:13:41 +0700

    Done

--- Soal1.c
+++ Soal1.c
@@ -24,6 +24,40 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
 }
 
 
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+{
+	char myDocPath[1000];
+	sprintf(myDocPath,"/home/%s/Documents",username);
+	if(strcmp(myDocPath,path)==0){
+		printf("IN THE DOC! %s\n",path);
+	}
+	//printf("currernt Path : %s by %s\n",myDocPath,path);
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+
+	dp = opendir(path);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		if (filler(buf, de->d_name, &st, 0))
+			break;
+	}
+
+	closedir(dp);
+
+	return 0;
+}
+
+
+
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset,struct fuse_file_info *fi)
 {
 	int fd;
